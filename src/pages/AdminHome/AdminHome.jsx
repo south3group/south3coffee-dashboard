0c82374a9f345755be08139c89862c8ddffa9011
LEO CHOU <leotimjack@gmail.com>
2025-06-22 20:28:02 +0800

    feat: integrate API data in AdminHome

--- src/pages/AdminHome/AdminHome.jsx
+++ src/pages/AdminHome/AdminHome.jsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import {  useEffect, useState } from 'react';
+import { useNavigate, Link } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { checkAuth, logout } from '../../store/authSlice';
 import axios from 'axios';
